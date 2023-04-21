f08e0a1c190461140b107a401c53ab947061b9d7
Joel Drapper <joel@drapper.me>
2023-04-21 10:51:54 +0100

    Improve custom matcher tests

--- test/matchers/custom_matcher.rb
+++ test/matchers/custom_matcher.rb
@@ -6,12 +6,20 @@ module FooMatcher
 	end
 end
 
-include_matcher FooMatcher
+include_matcher Matchers::PassFail
 
 test "custom matcher success case" do
-	expect("foo").foo?
+	expect {
+		include_matcher FooMatcher
+
+		test { expect("foo").foo? }
+	}.to_pass
 end
 
-test("custom matcher fail case", skip: true) do
-	expect("baz").foo?
+test("custom matcher fail case") do
+	expect {
+		include_matcher FooMatcher
+
+		test { expect("bar").foo? }
+	}.to_fail message: %(Expected bar to be "foo" but it wasn't.)
 end
