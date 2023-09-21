015e35b81c99166e9bad8144977bc944a28aee90
Joel Drapper <joel@drapper.me>
2023-09-21 10:58:28 +0100

    Working

--- lib/green_dots/cluster.rb
+++ lib/green_dots/cluster.rb
@@ -2,30 +2,14 @@
 
 class GreenDots::Cluster
 	def initialize
-		@pids = []
-		@pipes = []
+		@workers = []
 	end
 
-	def fork
-		reader, writer = IO.pipe
-
-		@pipes << reader
-
-		@pids << Process.fork do
-			reader.close
-			yield(writer)
-			writer.close
-		end
-
-		writer.close
+	def fork(&)
+		@workers << GreenDots::Worker.fork(&)
 	end
 
 	def wait
-		freeze
-		@pids.each { |pid| Process.wait(pid) }
-
-		@pipes.map do |reader|
-			reader.read.tap { reader.close }
-		end
+		@workers.map(&:wait)
 	end
 end
---
+++ lib/green_dots/worker.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+class GreenDots::Worker
+	def self.fork(&)
+		reader, writer = IO.pipe
+
+		pid = Process.fork do
+			reader.close
+			# TODO: this could be a writer that serializes the input
+			yield(writer)
+			writer.close
+		end
+
+		writer.close
+
+		new(pid:, reader:)
+	end
+
+	def initialize(pid:, reader:)
+		@pid = pid
+		@reader = reader
+	end
+
+	def wait
+		Process.wait(@pid)
+		@reader.read.tap { @reader.close }
+	end
+end
