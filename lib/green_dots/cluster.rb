6e1cb30aa7d957a3d1132957c71acc5aba51a39b
Joel Drapper <joel@drapper.me>
2023-09-21 11:01:04 +0100

    Working

--- lib/green_dots/cluster.rb
+++ lib/green_dots/cluster.rb
@@ -1,6 +1,16 @@
 # frozen_string_literal: true
 
 class GreenDots::Cluster
+	def self.call(n, &)
+		spawn(n, &).wait
+	end
+
+	def self.spawn(n, &)
+	  new.tap do |cluster|
+			n.times { cluster.fork(&) }
+		end
+	end
+
 	def initialize
 		@workers = []
 	end
