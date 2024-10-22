790190818d435d973e23e7f8319ab386c299e1eb
Joel Drapper <joel@drapper.me>
2024-10-23 00:19:09 +0100

    Update pipe.test.rb

--- test/pipe.test.rb
+++ test/pipe.test.rb
@@ -1,15 +1,17 @@
 # frozen_string_literal: true
 
-test "with_reader and with_writer" do
-	pipe = Quickdraw::Pipe.new
+if Process.respond_to?(:fork)
+	test "with_reader and with_writer" do
+		pipe = Quickdraw::Pipe.new
 
-	Process.fork do
-		pipe.with_writer do |writer|
-			writer.write "Hello, world!"
+		Process.fork do
+			pipe.with_writer do |writer|
+				writer.write "Hello, world!"
+			end
 		end
-	end
 
-	pipe.with_reader do |reader|
-		expect(reader.read) == "Hello, world!"
+		pipe.with_reader do |reader|
+			expect(reader.read) == "Hello, world!"
+		end
 	end
 end
